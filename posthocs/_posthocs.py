ecbe0bf9a311f88374a487be5fa4acf26ddbd55d
maximtrp <maximtrp@gmail.com>
2017-12-23 19:37:22 +0300

    Wilcoxon test added

--- posthocs/_posthocs.py
+++ posthocs/_posthocs.py
@@ -1351,15 +1351,13 @@ def posthoc_tukey_hsd(x, g, alpha = 0.05):
 
 def posthoc_mannwhitney(x, val_col = None, group_col = None, use_continuity = True, alternative = 'two-sided', p_adjust = None, sort = True):
 
-    '''Pairwise comparisons with Mann-Whitney rank test. This is also known
-    as pairwise two-sample Wilcoxon test.
+    '''Pairwise comparisons with Mann-Whitney rank test.
 
         Parameters
         ----------
         x : array_like or pandas DataFrame object
             An array, any object exposing the array interface or a pandas
-            DataFrame. Array must be two-dimensional. Second dimension may
-            vary, i.e. groups may have different lengths.
+            DataFrame. Array must be two-dimensional.
 
         val_col : str, optional
             Must be specified if x is a pandas DataFrame object.
@@ -1401,6 +1399,10 @@ def posthoc_mannwhitney(x, val_col = None, group_col = None, use_continuity = Tr
         -------
         Numpy ndarray if x is an array-like object else pandas DataFrame of p values.
 
+        Notes
+        -----
+        Refer to `scipy.stats.mannwhitneyu` reference page for further details.
+
         Examples
         --------
 
@@ -1440,6 +1442,115 @@ def posthoc_mannwhitney(x, val_col = None, group_col = None, use_continuity = Tr
 
     for i,j in combs:
         vs[i, j] = ss.mannwhitneyu(x_grouped[i], x_grouped[j], use_continuity=use_continuity, alternative=alternative)[1]
+
+    if p_adjust:
+        vs[tri_upper] = multipletests(vs[tri_upper], method = p_adjust)[1]
+    vs[tri_lower] = vs.T[tri_lower]
+    np.fill_diagonal(vs, -1)
+
+    if isinstance(x, DataFrame):
+        groups_unique = x[group_col].unique()
+        return DataFrame(vs, index=groups_unique, columns=groups_unique)
+    else:
+        return vs
+
+def posthoc_wilcoxon(x, val_col = None, group_col = None, zero_method='wilcox', correction=False, p_adjust = None, sort = True):
+
+    '''Pairwise comparisons with Wilcoxon signed-rank test. It is a non-parametric
+    version of the paired T-test.
+
+        Parameters
+        ----------
+        x : array_like or pandas DataFrame object
+            An array, any object exposing the array interface or a pandas
+            DataFrame. Array must be two-dimensional.
+
+        val_col : str, optional
+            Must be specified if x is a pandas DataFrame object.
+            The name of a column that contains values.
+
+        group_col : str, optional
+            Must be specified if x is a pandas DataFrame object.
+            The name of a column that contains group names.
+
+        zero_method : string, {"pratt", "wilcox", "zsplit"}, optional
+            "pratt": Pratt treatment: includes zero-differences in the ranking
+                process (more conservative)
+            "wilcox": Wilcox treatment: discards all zero-differences
+            "zsplit": Zero rank split: just like Pratt, but spliting the zero rank
+                between positive and negative ones
+
+        correction : bool, optional
+            If True, apply continuity correction by adjusting the Wilcoxon rank
+            statistic by 0.5 towards the mean value when computing the z-statistic.
+            Default is False.
+
+        p_adjust : str, optional
+            Method for adjusting p values.
+            See statsmodels.sandbox.stats.multicomp for details.
+            Available methods are:
+                'bonferroni' : one-step correction
+                'sidak' : one-step correction
+                'holm-sidak' : step-down method using Sidak adjustments
+                'holm' : step-down method using Bonferroni adjustments
+                'simes-hochberg' : step-up method  (independent)
+                'hommel' : closed method based on Simes tests (non-negative)
+                'fdr_bh' : Benjamini/Hochberg  (non-negative)
+                'fdr_by' : Benjamini/Yekutieli (negative)
+                'fdr_tsbh' : two stage fdr correction (non-negative)
+                'fdr_tsbky' : two stage fdr correction (non-negative)
+
+        sort : bool, optional
+            Specifies whether to sort DataFrame by group_col or not. Recommended
+            unless you sort your data manually.
+
+        Returns
+        -------
+        Numpy ndarray if x is an array-like object else pandas DataFrame of p values.
+
+        Notes
+        -----
+        Refer to `scipy.stats.wilcoxon` reference page for further details.
+
+        Examples
+        --------
+
+        >>> x = [[1,2,3,4,5], [35,31,75,40,21], [10,6,9,6,1]]
+        >>> ph.posthoc_wilcoxon(x)
+        array([[-1.       ,  0.0357757,  0.114961 ],
+               [ 0.0357757, -1.       ,  0.0357757],
+               [ 0.114961 ,  0.0357757, -1.       ]])
+
+    '''
+
+    if isinstance(x, DataFrame):
+        if not sort:
+            x[group_col] = Categorical(x[group_col], categories=x[group_col].unique(), ordered=True)
+
+        x.sort_values(by=[group_col, val_col], ascending=True, inplace=True)
+        x_lens = x.groupby(by=group_col)[val_col].count().values
+        x_lens_cumsum = np.insert(np.cumsum(x_lens), 0, 0)[:-1]
+        x_grouped = np.array([x[val_col][j:(j + x_lens[i])] for i, j in enumerate(x_lens_cumsum)])
+
+    else:
+        x = np.array(x)
+        x_grouped = np.array([np.asarray(a)[~np.isnan(a)] for a in x])
+        x_lens = np.asarray([len(a) for a in x_grouped])
+        x_lens_cumsum = np.insert(np.cumsum(x_lens), 0, 0)[:-1]
+
+    if any(x_lens == 0):
+        raise ValueError("All groups must contain data")
+
+    x_len = len(x_grouped)
+    vs = np.arange(x_len, dtype=np.float)[:,None].T.repeat(x_len, axis=0)
+    tri_upper = np.triu_indices(vs.shape[0], 1)
+    tri_lower = np.tril_indices(vs.shape[0], -1)
+    vs[:,:] = 0
+
+    combs = it.combinations(range(x_len), 2)
+
+    for i,j in combs:
+        vs[i, j] = ss.wilcoxon(x_grouped[i], x_grouped[j], zero_method=zero_method, correction=correction)[1]
 
     if p_adjust:
         vs[tri_upper] = multipletests(vs[tri_upper], method = p_adjust)[1]
