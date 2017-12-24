8807a8a0dfb6f78b9b69b3c1ea1dbad8dabf9e6a
maximtrp <maximtrp@gmail.com>
2017-12-25 01:16:20 +0300

    fixes in name

--- scikit_posthocs/__init__.py
+++ scikit_posthocs/__init__.py
@@ -1,13 +1,13 @@
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 
-from scikit-posthocs._posthocs \
+from scikit_posthocs._posthocs \
     import posthoc_conover, posthoc_dunn, posthoc_nemenyi,\
            posthoc_ttest, posthoc_tukey_hsd, posthoc_mannwhitney,\
            posthoc_nemenyi_friedman, posthoc_conover_friedman,\
            posthoc_quade, posthoc_durbin, posthoc_vanwaerden
 
-from scikit-posthocs._plotting \
+from scikit_posthocs._plotting \
     import sign_array, sign_plot, sign_table
 
-from scikit-posthocs._outliers \
+from scikit_posthocs._outliers \
     import outliers_iqr, outliers_grubbs, outliers_tietjen, outliers_gesd
