a327da0c84c57e34f64526e9e4961e27af77a814
maximtrp <maximtrp@gmail.com>
2018-01-06 01:41:09 +0300

    test template added

---
+++ tests/tests_posthocs.py
@@ -0,0 +1,44 @@
+import unittest as ut
+import scikit_posthocs as sp
+
+class TestPosthocs(ut.TestCase):
+
+    def test_posthoc_conover(self):
+        self.assertEqual()
+
+    def test_posthoc_dunn(self):
+        self.assertEqual()
+
+    def test_posthoc_nemenyi(self):
+        self.assertEqual()
+
+    def test_posthoc_nemenyi_friedman(self):
+        self.assertEqual()
+
+    def test_posthoc_conover_friedman(self):
+        self.assertEqual()
+
+    def test_posthoc_durbin(self):
+        self.assertEqual()
+
+    def test_posthoc_quade(self):
+        self.assertEqual()
+
+    def test_posthoc_vanwaerden(self):
+        self.assertEqual()
+
+    def test_posthoc_ttest(self):
+        self.assertEqual()
+
+    def test_posthoc_tukey_hsd(self):
+        self.assertEqual()
+
+    def test_posthoc_mannwhitney(self):
+        self.assertEqual()
+
+    def test_posthoc_wilcoxon(self):
+        self.assertEqual()
+
+
+if __name__ == '__main__':
+    ut.main()
