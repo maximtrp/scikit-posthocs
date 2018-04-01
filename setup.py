c73bdbca6380e5f3f5d51f01f242e097da38a28c
maximtrp <maximtrp@gmail.com>
2018-04-01 18:18:14 +0300

    New ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from os.path import join, dirname
 
 setup(name='scikit-posthocs',
-      version='0.3.5',
+      version='0.3.6',
       description='Statistical post-hoc analysis and outlier detection algorithms',
       long_description=open(join(dirname(__file__), 'README.rst')).read(),
       url='http://github.com/maximtrp/scikit-posthocs',
