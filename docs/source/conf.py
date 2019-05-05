4e20b815d0db0783811c33fb5125cb610841b90b
maximtrp <maximtrp@gmail.com>
2019-05-05 10:13:46 +0300

    docs conf upd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,10 +13,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-import scikit_posthocs
-import sphinx_rtd_theme
+#import os
+#import sys
+#import scikit_posthocs
+#import sphinx_rtd_theme
 
 #sys.path.insert(0, os.path.abspath('../../'))
 
