f112ad9cfb395a2e56fef0417422ea56113b1cbd
maximtrp <maximtrp@gmail.com>
2019-12-11 15:49:16 +0300

    imports upd

--- scikit_posthocs/__init__.py
+++ scikit_posthocs/__init__.py
@@ -2,7 +2,7 @@ __version__ = '0.6.1'
 
 from scikit_posthocs._omnibox import test_osrt, test_durbin, test_mackwolfe # noqa
 
-from scikit_posthocs._posthocs \ # noqa
+from scikit_posthocs._posthocs \
 import posthoc_anderson, posthoc_conover, posthoc_conover_friedman,\
     posthoc_dscf, posthoc_dunn, posthoc_durbin,\
     posthoc_mannwhitney, posthoc_miller_friedman, posthoc_nemenyi,\
@@ -10,7 +10,7 @@ import posthoc_anderson, posthoc_conover, posthoc_conover_friedman,\
     posthoc_scheffe, posthoc_siegel_friedman, posthoc_tamhane,\
     posthoc_ttest, posthoc_tukey, posthoc_tukey_hsd,\
     posthoc_vanwaerden, posthoc_wilcoxon, __convert_to_df,\
-    __convert_to_block_df
+    __convert_to_block_df # noqa
 
 from scikit_posthocs._plotting import sign_array, sign_plot, sign_table # noqa
 from scikit_posthocs._outliers import outliers_gesd, outliers_grubbs, outliers_iqr, outliers_tietjen # noqa
