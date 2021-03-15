c6093234688f9ea0873e336423ac1ffa0f520477
Maksim Terpilowski <maximtrp@gmail.com>
2021-03-15 19:04:05 +0300

    bugfix release

--- scikit_posthocs/__init__.py
+++ scikit_posthocs/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.7.0'
+__version__ = '0.6.7'
 
 from scikit_posthocs._global import global_simes_test, global_f_test  # noqa
 from scikit_posthocs._omnibus import test_osrt, test_durbin, test_mackwolfe  # noqa
